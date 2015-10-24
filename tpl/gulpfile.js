99701245b462dbda0d15cac886b9fe3d9f08da62
i5ting <i5ting@126.com>
2015-10-24 16:10:31 +0800

    use i5ting_doc for generate

--- tpl/gulpfile.js
+++ tpl/gulpfile.js
@@ -17,17 +17,35 @@ gulp.task('rename',function () {
 	}	
 });
 
-// 使用i5ting_toc直接生成，不再使用shell
-// copy img到preview下面
-gulp.task('generate',function () {
-	// Run external tool synchronously
-	if (exec('sh ./generate.sh').code !== 0) {
-	  echo('Error: generate.sh exec failed');
-	  exit(1);
-	}	
+gulp.task('copy_img',function () {
+  return gulp.src('./img/**/*')
+      .pipe(gulp.dest('./preview/img'));
 });
 
-gulp.task('show',['generate'] ,function () {
+// 使用i5ting_toc直接生成，不再使用shell
+// copy img到preview下面
+gulp.task('doc', ['copy_img'],function () {
+  var is_open = true;
+  var markd_config = {
+  	debug: false
+  }
+  //函数可以返回当前正在执行的项目路径
+  var pwd = process.cwd()  
+  var source_file = "README.md"
+  var source_file_name = pwd + '/' + source_file
+  var file_name = source_file_name.split('/').pop();;
+  var _file_name = file_name.split('.')[0];
+
+  var dest_file_path = pwd + '/preview/' + _file_name + '.html';
+
+  console.log('pwd=' + pwd);
+  console.log('source_file_name=' + source_file_name);
+  console.log('dest_file_path=' + dest_file_path);
+
+  require('i5ting_toc')(pwd, source_file_name, dest_file_path, is_open, markd_config);
+});
+
+gulp.task('show',['doc'] ,function () {
     console.log('show');
 });
 
